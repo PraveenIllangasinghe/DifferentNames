15e11e81ef35f4e081698588b0853627e814ebb1
PraveenIllangasinghe <praveenillangasinghe99@gmail.com>
2020-09-16 09:57:09 +0530

    initial commit of shop2

---
+++ app/src/main/java/com/example/shop2/PrimaryActivity.java
@@ -0,0 +1,16 @@
+package com.example.shop2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrimaryActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary);
+    }
+}
